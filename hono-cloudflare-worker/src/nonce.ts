389ed27f64c7bdfa9a03f380880b2fd6ce124bce
Tony D'Addeo <amdaddeo@gmail.com>
2025-05-21 12:20:32 -0500

    wait on nonce, too many race conditions

--- hono-cloudflare-worker/src/nonce.ts
+++ hono-cloudflare-worker/src/nonce.ts
@@ -11,10 +11,12 @@ export async function generateNonce(): Promise<string> {
 }
 
 export async function storeNonce(env: Cloudflare.Env, nonce: string): Promise<void> {
+  const start = Date.now();
   const nonceId = env.NONCE.idFromName(nonce);
   const nonceObject = env.NONCE.get(nonceId);
 
   await nonceObject.initialize();
+  console.log("storeNonce duration", Date.now() - start);
 }
 
 export async function consumeNonce(env: Cloudflare.Env, nonce: string): Promise<boolean> {
