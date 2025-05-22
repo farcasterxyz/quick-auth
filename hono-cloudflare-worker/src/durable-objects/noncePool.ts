b65e45d0562953e996d0f571bc2758ec043635f7
Tony D'Addeo <amdaddeo@gmail.com>
2025-05-22 12:05:41 -0500

    add index on expires_at

--- hono-cloudflare-worker/src/durable-objects/noncePool.ts
+++ hono-cloudflare-worker/src/durable-objects/noncePool.ts
@@ -7,16 +7,19 @@ export class NoncePool extends DurableObject<Cloudflare.Env> {
     super(ctx, env)
     this.sql = ctx.storage.sql;
 
-    this.sql.exec(`CREATE TABLE IF NOT EXISTS nonces(
-      nonce    TEXT PRIMARY KEY,
-      expires_at INTEGER NOT NULL
-    );`)
+    this.sql.exec(`
+      CREATE TABLE IF NOT EXISTS nonces(
+        nonce      TEXT PRIMARY KEY,
+        expires_at INTEGER NOT NULL
+      );
+      CREATE INDEX IF NOT EXISTS idx_nonces_expires_at ON nonces(expires_at);
+    `)
   }
 
   async store(nonce: string): Promise<boolean> {
     const expiresAt = Date.now() + 5 * 60 * 1000;
 
-    this.sql.exec(`INSERT INTO NONCES (nonce, expires_at) VALUES ('${nonce}', ${expiresAt});`);
+    this.sql.exec(`INSERT INTO nonces (nonce, expires_at) VALUES (?, ?);`, nonce, expiresAt);
     this.scheduleCleanup();
 
     return true;
@@ -25,9 +28,9 @@ export class NoncePool extends DurableObject<Cloudflare.Env> {
   // Consume the nonce if it's valid
   async consume(nonce: string): Promise<boolean> {
     const cursor = this.sql.exec<{ expires_at: number }>(`DELETE FROM nonces 
-      WHERE nonce = '${nonce}' 
+      WHERE nonce = ?
       RETURNING expires_at;
-    `)
+    `, nonce)
 
     const row = cursor.next();
     if (!row.value || row.value.expires_at < Date.now()) {
@@ -49,6 +52,6 @@ export class NoncePool extends DurableObject<Cloudflare.Env> {
   }
 
   private async cleanup(): Promise<void> {
-    this.sql.exec(`DELETE FROM NONCES WHERE expires_at <= ${Date.now() / 1000}`)
+    this.sql.exec(`DELETE FROM nonces WHERE expires_at <= ?`, Date.now())
   }
 }
